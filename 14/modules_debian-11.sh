6c04c245c0527fb99b112887528559fbe9935cfd
Ignacio Buioli <ibuioli@gmail.com>
2022-07-27 22:48:43 -0300

    feat: add l10n_ar_payroll

--- 14/modules_debian-11.sh
+++ 14/modules_debian-11.sh
@@ -7,6 +7,7 @@ sudo mkdir sources
 cd sources
 sudo git clone https://github.com/codize-app/odoo-argentina -b 14.0
 sudo git clone https://github.com/OCA/reporting-engine -b 14.0
+sudo git clone https://github.com/OCA/web -b 14.0
 sudo git clone https://github.com/OCA/account-reconcile -b 14.0
 sudo git clone https://github.com/ExemaxSAS/helpdesk_button -b 14.0
 sudo git clone https://github.com/CybroOdoo/CybroAddons -b 14.0
@@ -28,6 +29,7 @@ sudo mkdir cache
 sudo chmod -R 777 cache
 
 echo ADDONS_PATH:
-sudo sed -i 's/\/opt\/odoo\/addons/\/opt\/odoo\/addons, \/opt\/odoo\/sources, \/opt\/odoo\/sources\/odoo-argentina, \/opt\/odoo\/sources\/odoo-argentina-withholding, \/opt\/odoo\/sources\/account-reconcile, \/opt\/odoo\/sources\/CybroAddons, \/opt\/odoo\/sources\/reporting-engine, \/opt\/odoo\/sources\/om_account_accountant/g' /etc/odoo.conf
+sudo sed -i 's/\/opt\/odoo\/addons/\/opt\/odoo\/addons, \/opt\/odoo\/sources, \/opt\/odoo\/sources\/web, \/opt\/odoo\/sources\/odoo-argentina, \/opt\/odoo\/sources\/odoo-argentina-withholding, \/opt\/odoo\/sources\/account-reconcile, \/opt\/odoo\/sources\/CybroAddons, \/opt\/odoo\/sources\/reporting-engine, \/opt\/odoo\/sources\/om_account_accountant/g' /etc/odoo.conf
 echo /opt/odoo/sources, /opt/odoo/sources/odoo-argentina, /opt/odoo/sources/odoo-argentina-withholding, /opt/odoo/sources/account-reconcile, /opt/odoo/sources/CybroAddons, /opt/odoo/sources/reporting-engine, /opt/odoo/sources/om_account_accountant
 echo RESTARTING ODOO SERVER
+sudo service odoo restart
